ea8a7f384a65ef50a345d426f5f6f73a2ce43d0e
Steveatron3000 <j.r.franklin@aol.com>
2020-10-12 13:39:26 -0500

    Ready

---
+++ Exercise1.js
@@ -0,0 +1,57 @@
+// Fav Number
+console.log(21);
+// 5 Primatives
+
+// 1: Numbers
+// 2:Strings
+// 3:Booleans
+// 4:Null
+// 5:Unidentified
+
+//Symbols:
+// 1: ()
+// 2: %
+// 3: ;
+// 4: ""
+// 5: \
+
+//Modulo, 3 from 7:
+console.log(7%4);
+console.log(10%7);
+
+//Mary Poppins
+console.log('I\'m MARY POPPINS, YALL!');
+
+//Spider-Man
+console.log("Spider"+"-"+"man")
+
+//Name Length
+console.log("Jennings".length+"Franklin".length)
+
+// E at the end of the string
+console.log("Avengers: Endgame"[16])
+
+//BONUS
+// Backslash
+console.log(' \\ ')
+
+//616 from 5, 4, 9
+
+console.log(5**4 -9);
+
+//I AM ZERO MAN
+
+console.log("I".length+"IRON".length-"AM".length-"MAN".length)
+
+//AVENGERS (Cool)
+
+console.log("CAP"[1])
+console.log("VISION"[0])
+console.log("HAWKEYE"[4])
+console.log("NICK FURY"[0])
+console.log("GROOT"[0])
+console.log("DOCTOR STRANGE"[13])
+console.log("THOR"[3])
+console.log("STAR-LORD"[0])
+
+//Skipped last Bonus....
